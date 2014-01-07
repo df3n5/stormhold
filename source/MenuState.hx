bb0078dba25bfc484cf41602bb5030956179bcdf
Jonathan Frawley <jonathanfrawley@gmail.com>
2014-01-07 12:34:34 +0000

    Indentation

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -9,25 +9,18 @@ import flixel.util.FlxMath;
 import flixel.tweens.misc.ColorTween;
 import flixel.tweens.FlxTween;
 
-/**
- * A FlxState which can be used for the game's menu.
- */
 class MenuState extends FlxState {
     private var tween:ColorTween;
     private var text:FlxText;
     private var titleText:FlxText;
 
-	/**
-	 * Function that is called up when to state is created to set it up. 
-	 */
-	override public function create():Void {
-		// Set a background color
-		FlxG.cameras.bgColor = 0xff131c1b;
-		// Show the mouse (in case it hasn't been disabled)
-		#if !FLX_NO_MOUSE
-		FlxG.mouse.show();
-		#end
-		
+    override public function create():Void {
+        FlxG.cameras.bgColor = 0xff131c1b;
+        // Show the mouse (in case it hasn't been disabled)
+        #if !FLX_NO_MOUSE
+        FlxG.mouse.show();
+        #end
+        
         var title = new FlxSprite(0, 0, "assets/images/title.png");
         add(title);
 
@@ -41,25 +34,18 @@ class MenuState extends FlxState {
         text.setFormat("assets/REVOLUTION.ttf", 38, 0xFFabcd, "center", FlxText.BORDER_OUTLINE);
         add(text);
         tween = FlxTween.color(2, 0, 0, 1, 0.1, { type: FlxTween.PINGPONG });
-		super.create();
-	}
+        super.create();
+    }
 
-	/**
-	 * Function that is called when this state is destroyed - you might want to 
-	 * consider setting all objects this state uses to null to help garbage collection.
-	 */
-	override public function destroy():Void {
-		super.destroy();
-	}
+    override public function destroy():Void {
+        super.destroy();
+    }
 
-	/**
-	 * Function that is called once every frame.
-	 */
-	override public function update():Void {
-		super.update();
+    override public function update():Void {
+        super.update();
         text.alpha = tween.alpha;
         if(FlxG.keyboard.pressed("SPACE")) {
             FlxG.switchState(new PlayState());
         }
-	}	
+    }   
 }
